e8a2c825efd49d38e1047d75238886302bef0ef9
Renee Dedula <dedulara@mail.uc.edu>
2023-04-23 23:21:27 -0400

    Initial commit

---
+++ src/RecursiveListerRunner.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class RecursiveListerRunner
+{
+    public static void main(String[] args)
+    {
+        JFrame frameFrame = new RecursiveListerMaker();
+
+        frameFrame.setTitle("File Lister");
+    }
+}
